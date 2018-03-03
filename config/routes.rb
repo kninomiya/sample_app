a99d8ae39b1b98164ba333bf551a0a1b6b3fd4c8
EC2 Default User <ec2-user@ip-172-31-35-229.us-east-2.compute.internal>
2018-03-03 10:20:47 +0000

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
+SampleApp::Application.routes.draw do
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   
-  get 'static_pages/about'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
