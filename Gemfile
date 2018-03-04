b0460020b5efedb39760c7b4aa6d1be8f97d1683
EC2 Default User <ec2-user@ip-172-31-35-229.us-east-2.compute.internal>
2018-03-04 04:06:47 +0000

    Install the devise

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
 end
 gem 'bootstrap-sass', '3.3.1'
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  
+#root to: "home#index"  
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
