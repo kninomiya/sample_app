98fba69edbe9f913a2bfff7836f310d967e8da3f
EC2 Default User <ec2-user@ip-172-31-35-229.us-east-2.compute.internal>
2018-03-20 08:51:12 +0000

    Finish user follow /unfollow actions-bug1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,22 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
-
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   private
+  
 
   def admin_user
     redirect_to(root_path) unless current_user.admin?
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,17 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where('user_id = ?', id)
   end
-  
-  has_many :microposts, dependent: :destory
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  
+
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
-  
+
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
@@ -29,4 +29,8 @@ class User < ApplicationRecord
     relationships.create!(followed_id: other_user.id)
   end
 
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 end
